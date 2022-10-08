0e7af4fd76da3f312e6c6fc8a7ec2d6b27c660af
Sabeer ali <sabeer@sectorqube.com>
2022-10-08 18:44:34 +0530

    add ssl certs

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const https = require("https");
+const fs = require("fs");
 
 app.get("/", (req, res) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log("Server running on port 4000");
+
+const httpsOptions = {
+  cert: fs.readFileSync("./certs/fullchain1.pem"),
+  key: fs.readFileSync("./certs/privkey1.pem"),
+};
+
+https.createServer(httpsOptions, app).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//   console.log("Server running on port 4000");
+// });
